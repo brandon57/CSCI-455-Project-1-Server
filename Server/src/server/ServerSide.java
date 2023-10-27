d6037e3cd2146ffd449fb7cf1e00364ccf69ba11
brandon57 <bmsitarz@gmail.com>
2023-10-26 20:37:49 -0500

    Added the connect and validPort methods from the client application

--- Server/src/server/ServerSide.java
+++ Server/src/server/ServerSide.java
@@ -8,41 +8,36 @@ import java.util.Scanner;
 public class ServerSide {
 	
 	private static ServerSocket User_Socket = null;
+	private static Scanner input = new Scanner(System.in);
+	private static ExecutorService Thread_Manager = Executors.newCachedThreadPool();
+	
 	
 	public static void main(String[] args) throws Exception {
 		
-		Scanner input = new Scanner(System.in);
+		
 		String inputText = "";
 		
 		//Manages the threads
-		ExecutorService Thread_Manager = Executors.newCachedThreadPool();
 		
-		//test
 		
 		//Creates a socket
-		while(true)
-		{
-			try
-			{
-				//System.out.println("Set the ip");
-				//inputText = input.
-				System.out.println("Set the port number");
-				inputText = input.nextLine();
-				System.out.println("Opening Socket...");
-				User_Socket = new ServerSocket(Integer.valueOf(inputText));
-				System.out.println("Socket is open");
-				break;
-	
-			}
-			catch(NumberFormatException n)
-			{
-				System.out.println("Your input has to be a number\nTry again");
-			}
-			catch(IOException i)
-			{
-				System.out.println("Couldn't open socket\nTry again");
-			}
-		}
+		connect();
+//		while(true)
+//		{
+//			try
+//			{	
+//				connect();
+//				break;
+//			}
+//			catch(NumberFormatException n)
+//			{
+//				System.out.println("Your input has to be a number\nTry again");
+//			}
+//			catch(IOException i)
+//			{
+//				System.out.println("Couldn't open socket\nTry again");
+//			}
+//		}
 		
 		Database Fundraisers = new Database();
 		
@@ -54,8 +49,61 @@ public class ServerSide {
 		}
 	}
 	
-	private void socketSetup(String input)
+	private static void connect() throws Exception
 	{
-
+		String port = null;
+		while(true)
+		{
+			System.out.println("What is the port number of the server? Your input should be between 0-65535\nYou can exit if you want");
+			port = input.nextLine();
+			if(validPort(port) == true)
+			{
+				break;
+			}
+			else if(port.equalsIgnoreCase("exit"))
+			{
+				close();
+			}
+			else
+			{
+				System.out.println("That port number is not valid");
+			}
+		}
+		System.out.println("Opening Socket...");
+		User_Socket = new ServerSocket(Integer.valueOf(port));
+		System.out.println("Socket is open");
+	}
+	
+	private static boolean validPort(String port)
+	{
+		Integer temp = 0;
+		try
+		{
+			temp = Integer.valueOf(port);
+			if(temp >= 0 && temp <= 65535)
+			{
+				return true;
+			}
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		return false;
+	}
+	
+	private static void close() throws Exception
+	{
+		if(User_Socket != null)
+		{
+			User_Socket.close();
+		}
+		if(input != null)
+		{
+			input.close();
+		}
+		Thread_Manager.isShutdown();
+		System.out.println("Shuting down...");
+		System.exit(0);
 	}
 }
