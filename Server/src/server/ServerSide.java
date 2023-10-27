1ac476e9379a1a05d748a25f88175dfb49a11a41
brandon57 <bmsitarz@gmail.com>
2023-10-26 19:07:47 -0500

    Again

--- Server/src/server/ServerSide.java
+++ Server/src/server/ServerSide.java
@@ -17,7 +17,7 @@ public class ServerSide {
 		//Manages the threads
 		ExecutorService Thread_Manager = Executors.newCachedThreadPool();
 		
-		URL server = new URI("http://test").toURL();
+		//URL server = new URI("http://test").toURL();
 		//Creates a socket
 		while(true)
 		{
